eadab0f0464f903e7bba426c81082120c5d7657c
kaseyshilt <kaseyshilt@outlook.com>
2020-12-02 15:08:41 -0600

    :added feature 8 and 9

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { ToDoBanner } from './TODOBANNER';
 import 'bootstrap/dist/css/bootstrap.css';
 import { ToDoRow } from './TODOROW';
 import { ToDoCreator } from './TODOCREATOR';
+import {VisibilityControl} from './VISIBILITYCONTROL';
 
 export default class App extends Component {
   //  Above we have created a class called App the extends the functionality of the Component class
@@ -45,7 +46,8 @@ export default class App extends Component {
       todoList: this.state.todoList.map(
         bob => bob.action === checkedToDoItem.action ? { ...bob, done: !bob.done } : bob
       )
-    }
+    }, () => localStorage.setItem("storedToDoObject", JSON.stringify(this.state)
+    )
   );
 
   //  Feature 5d
@@ -64,57 +66,94 @@ export default class App extends Component {
             { action: newToDoAction, done: false }
           ]
           // By default every new todo should not be done- in other words it's done property should have a value of false.
-        }
-      )
-    }
-
+        }, () => localStorage.setItem("storedToDoObject", JSON.stringify(this.state)
+        )//end of set item
+      )//end of setstate
+    }// end of if block
   }
-
   // Feature 5e
   //  The componentDidMount method below is a built in react method to handle logic for when the APP Component "mounts" or "loads"
-  componentDidMount = () =>
-  {
-    
-  }
+  componentDidMount = () => {
+    localStorage.clear();
 
-  render = () =>
-    <div>
-      {/* features 1 & 2 */}
-      <ToDoBanner
-        todoOwner={this.state.todoOwner}
-        todoList={this.state.todoList}
-      />
-
-      {/* feature 5a */}
-      <ToDoCreator
-        callback={this.createNewToDoCallback}
-      />
-
-      {/* features 3 and 4 */}
-      <table className="table table-striped table-bordered">
-        <thead>
-          <th>Action</th>
-          <th>Mark As Complete</th>
-        </thead>
-        <tbody>
-          {this.todoTableRows(false)}
-        </tbody>
-      </table>
+    fetch("http://localhost:56030/api/todos?todoOwnerID=1")
+      .then(response => response.json())
+      .then((data) => {
+        console.log(JSON.stringify({ data }));
 
 
-      {/* features 6 and 7 */}
-      <table className="table table-striped table-bordered">
-        <thead>
-          <th>Action</th>
-          <th>Mark As Not Complete</th>
-        </thead>
-        <tbody>
-          {this.todoTableRows(true)}
-        </tbody>
-      </table>
-    </div>
+        var apiList = [];
 
-}// end of app
+        for (var i = 0; i < data.Data.length; i++) {
+          var isDone = true;
+          if (data.Data[i].done === 0) {
+            isDone = false;
+          }
+          var element = { action: data.Data[i].action, done: isDone };
+          apiList.push(element);
+        }
+
+        let storedData = localStorage.getItem("storedToDoObject");
+        this.setState(
+          storedData != null ? JSON.parse(storedData) :
+            {
+              todoOwner: data.Data[0].todoOwner,
+              todoList: apiList,
+              showCompleted: true
+            }
+        );
+      })
+      .catch(console.log)
+    }
+    render = () =>
+      <div>
+        {/* features 1 & 2 */}
+        <ToDoBanner
+          todoOwner={this.state.todoOwner}
+          todoList={this.state.todoList}
+        />
+
+        {/* feature 5a */}
+        <ToDoCreator
+          callback={this.createNewToDoCallback}
+        />
+
+        {/* features 3 and 4 */}
+        <table className="table table-striped table-bordered">
+          <thead>
+            <th>Action</th>
+            <th>Mark As Complete</th>
+          </thead>
+          <tbody>
+            {this.todoTableRows(false)}
+          </tbody>
+        </table>
+
+        {/* feature 8 */}
+        <div className = "bg-secondary text-white text-center p-2">
+          <VisibilityControl
+            description = "Completed Tasks"
+            isChecked = {this.state.showCompleted}
+            callback = {checked => this.setState({showCompleted: checked})}
+          />
+        </div>
+
+
+        {/* features 6 and 7 */}
+        {this.state.showCompleted && 
+        <table className="table table-striped table-bordered">
+          <thead>
+            <th>Action</th>
+            <th>Mark As Not Complete</th>
+          </thead>
+          <tbody>
+            {this.todoTableRows(true)}
+          </tbody>
+        </table>
+}
+      </div>
+
+  }// end of app
 
 /*
 function App() {
